46dbfd1e94bf2dd909935ef08f1bfa08e5c65e0a
martinippo01 <martinippo@gmail.com>
2021-11-29 19:46:24 -0300

    Modifications to front, MouseEvent modifications

--- src/frontend/MouseEvent.java
+++ src/frontend/MouseEvent.java
@@ -4,25 +4,22 @@ import backend.model.Point;
 
 public class MouseEvent {
 
-    private Point beginPoint = null, endPoint = null;
+    private Point startPoint = null, endPoint = null;
 
-    public MouseEvent(){
-
-    }
-
+    public MouseEvent(){}  //TODO ver si es necesario este contructor
 
     public MouseEvent(Point beginPoint){
-        this.beginPoint = beginPoint;
+        this.startPoint = beginPoint;
     }
-
 
     public void setBeginPoint(Point beginPoint) {
-        this.beginPoint = beginPoint;
+        this.startPoint = beginPoint;
     }
 
-    public void setEndPoint(Point endPoint){
+    public void setEndPoint(Point endPoint) throws IllegalStateException{
         this.endPoint = endPoint;
+        if(startPoint == null || endPoint.getX() <= startPoint.getX() || endPoint.getY() <= startPoint.getY())  // Le agregamos que no deben ser iguales (antes era < no <=), TODO asegurarnos de esto
+            throw new IllegalStateException("Invalid end point relative to start point");
     }
-
 
 }

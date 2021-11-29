34e99beec95c77509ebe4520fd45f4d8361b9a8f
marcoscilipoti <elscili444.code@gmail.com>
2021-11-29 12:59:25 -0300

    Commented the MainFrame class

--- src/frontend/MainFrame.java
+++ src/frontend/MainFrame.java
@@ -3,11 +3,18 @@ package frontend;
 import backend.CanvasState;
 import javafx.scene.layout.VBox;
 
+// Define el VBox principal que maneja AppMenuBar (el menu superior con la opcion de salir o about) y
+// el StatusPane (panel que aparece abajo de la pantalla con informacion sobre el current elemento
+// del UI o la posicion del cursor del mouse )
 public class MainFrame extends VBox {
 
     public MainFrame(CanvasState canvasState) {
-        getChildren().add(new AppMenuBar());
-        StatusPane statusPane = new StatusPane();
+
+        getChildren().add(new AppMenuBar());                        //Agrego el AppMenuBar al VBox para que se renderise
+
+        StatusPane statusPane = new StatusPane();                   //Creo un nuevo StatusPanel para mantener informacion en
+                                                                    // el VBox sobre la current figure o cursor del mouse
+
         getChildren().add(new PaintPane(canvasState, statusPane));
         getChildren().add(statusPane);
     }
--- src/frontend/StatusPane.java
+++ src/frontend/StatusPane.java
@@ -4,6 +4,9 @@ import javafx.geometry.Pos;
 import javafx.scene.control.Label;
 import javafx.scene.layout.BorderPane;
 
+///DONDE LLAMO A UPDATESTATUS? PODRIA TRABAJARLO DIRECTAMENTE DESDE ACA, SIMPLEMENTE
+//ES LA POSICION DEL MOUSE O SI ESTA DENTRO DE UNA FIGURA (CUYO CASO TAMBIEN TENDRIA
+//QUE ALMACENAR A CANVAS_STATE ACA)
 public class StatusPane extends BorderPane {
 
 	private final Label statusLabel;
@@ -15,7 +18,8 @@ public class StatusPane extends BorderPane {
 		statusLabel.setStyle("-fx-font-size: 16");
 		setCenter(statusLabel);
 	}
-	
+
+
 	public void updateStatus(String text) {
 		statusLabel.setText(text);
 	}

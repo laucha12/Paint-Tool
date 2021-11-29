875287ef7dd08c04289366b9278ddf6a038e1f8c
laucha12 <41192791+laucha12@users.noreply.github.com>
2021-11-29 19:24:24 -0300

    bastante

--- src/frontend/Elements/FrontCircle.java
+++ src/frontend/Elements/FrontCircle.java
@@ -10,7 +10,7 @@ import javafx.scene.canvas.GraphicsContext;
 public interface FrontCircle {
 
     default void print(GraphicsContext gc, Point centerPoint, double width, double length){
-        gc.fillOval(centerPoint.getX()-width/2, centerPoint.getY()-length/2, width,length);
-        gc.strokeOval(centerPoint.getX()-width/2, centerPoint.getY()-length/2,width,length);
+        gc.fillOval(centerPoint.getX()-(width/2), centerPoint.getY()-(length/2), width*2,length*2);
+        gc.strokeOval(centerPoint.getX()-(width/2), centerPoint.getY()-(length/2),width*2,length*2);
     }
 }

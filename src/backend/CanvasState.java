cc0d09818f80bdd799cedc916e0cdc0e0ae50710
martinippo01 <martinippo@gmail.com>
2021-11-29 13:38:22 -0300

    CanvasState commented

--- src/backend/CanvasState.java
+++ src/backend/CanvasState.java
@@ -7,12 +7,14 @@ import java.util.List;
 
 public class CanvasState {
 
+    //Guarda una lista con las figuras que estan presentes en el canvas
     private final List<Figure> list = new ArrayList<>();
 
     public void addFigure(Figure figure) {
         list.add(figure);
     }
 
+    //Me retorna un objeto iterable para que pueda recorrer sobre las distintas figuras
     public Iterable<Figure> figures() {
         return new ArrayList<>(list);
     }

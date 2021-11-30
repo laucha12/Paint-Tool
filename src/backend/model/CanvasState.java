33b28637711f6f51b8696c34729f45e138006e07
martinippo01 <martinippo@gmail.com>
2021-11-30 16:47:10 -0300

    Working on function number 4, (send to back, bring to front)

--- src/backend/model/CanvasState.java
+++ src/backend/model/CanvasState.java
@@ -3,13 +3,14 @@ package backend.model;
 import backend.model.Figure;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 
 public class CanvasState {
 
     //Guarda una lista con las figuras que estan presentes en el canvas
     private final List<Figure> list = new ArrayList<>();
-    //TIENE QUE SER UNA LINKED LIST PARA INSERTAR ADELANTE DE TODO, PUNTO 4
+    //TIENE QUE SER UNA LINKED LIST PARA INSERTAR ADELANTE DE TODO, PUNTO 4. Es necesario una linkedList?
 
     public void addFigure(Figure figure) {
         list.add(figure);
@@ -20,6 +21,34 @@ public class CanvasState {
         return new ArrayList<>(list);
     }
 
+    //Nuevos metodos creados:
+    public void sendFigureToBack(Figure figure){
+        list.remove(figure);
+        list.add(0,figure);
+    }
+
+    public void sendFigureToFront(Figure figure){
+        list.remove(figure);
+        list.add(list.size(), figure);
+    }
+
+    public void sendMultipleFiguresToBack(Collection<Figure> figures){
+        figures.forEach(this::sendFigureToBack);
+    }
+
+    public void sendMultipleFiguresToFront(Collection<Figure> figures){
+        figures.forEach(this::sendFigureToFront);
+    }
+
+
 }
 
 //PUNTO 3, BORRAR DEL CANVAS Y VOLVER A REDRAWEAR()
+
+
+
+/*
+* Modificaciones para el punto 4:
+*   Se cambia el ArrayList por un LinkedList. De esta forma podemos hacer que un elemento vaya al comienzo o al final de la lista.
+*
+* */

661ae601a3edaae105131025a596ea4363d5cdef
laucha12 <41192791+laucha12@users.noreply.github.com>
2021-12-01 12:07:44 -0300

    Errores varios

---
+++ src/frontend/engines/SelectedEngine.java
@@ -0,0 +1,11 @@
+package frontend.engines;
+
+import backend.model.components.Figure;
+
+import java.util.ArrayList;
+
+public class SelectedEngine {
+    private ArrayList<Figure> elements= new ArrayList<>();
+
+
+}

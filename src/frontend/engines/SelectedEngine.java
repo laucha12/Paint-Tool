296e240d751eace2322d2f2a6104a6d277c5035a
laucha12 <41192791+laucha12@users.noreply.github.com>
2021-12-01 12:10:38 -0300

    Errores varios

--- src/frontend/engines/SelectedEngine.java
+++ src/frontend/engines/SelectedEngine.java
@@ -3,9 +3,22 @@ package frontend.engines;
 import backend.model.components.Figure;
 
 import java.util.ArrayList;
+import java.util.Collection;
 
 public class SelectedEngine {
     private ArrayList<Figure> elements= new ArrayList<>();
 
-
+    public void addFigure(Figure other){
+        other.select();
+        elements.add(other);
+    }
+    public Collection<Figure> getSelected(){
+        return elements;
+    }
+    public void unselectAll(){
+        for (Figure aux : elements)
+            aux.resetStrokeColor();
+        //limpio las figuras seleccion
+        elements.clear();
+    }
 }

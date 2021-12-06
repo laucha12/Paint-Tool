8d2e40da0af638d07dda63e50f5e8c60e8cbb6b8
martinippo01 <martinippo@gmail.com>
2021-12-06 10:51:43 -0300

    Minor changes

--- src/frontend/engines/CanvasEngine.java
+++ src/frontend/engines/CanvasEngine.java
@@ -6,7 +6,7 @@ import javafx.scene.canvas.Canvas;
 
 public class CanvasEngine {
 
-    //Hecha abstracta asi la pueden llamar los engines
+    //Hecha de clase asi la pueden llamar los engines
     public static void redrawCanvas(CanvasState canvasState, Canvas canvas) {
         canvas.getGraphicsContext2D().clearRect(0, 0, canvas.getWidth(), canvas.getHeight());
         for (Figure figure : canvasState.figures())                                //al canvasState le pido las figuras e itero sobre ellas
